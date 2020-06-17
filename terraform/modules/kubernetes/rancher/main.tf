6aef026711d2df71873d68c7c57d46fda89d29a5
Jacob Weinstock <jacobweinstock@users.noreply.github.com>
2020-06-16 21:36:08 -0600

    get push to registry on master branch working (#6)
    * get push to registry on master branch working
    
    * fmt

--- terraform/modules/kubernetes/rancher/main.tf
+++ terraform/modules/kubernetes/rancher/main.tf
@@ -72,14 +72,14 @@ resource "local_file" "rkeconfig" {
 }
 
 resource "local_file" "ssh_private_key" {
-  filename = "${path.root}/deliverables/id_rsa"
-  content  = file(var.ssh-private-key)
+  filename        = "${path.root}/deliverables/id_rsa"
+  content         = file(var.ssh-private-key)
   file_permission = "400"
 }
 
 resource "local_file" "ssh_public_key" {
-  filename = "${path.root}/deliverables/id_rsa.pub"
-  content  = file(var.ssh-public-key)
+  filename        = "${path.root}/deliverables/id_rsa.pub"
+  content         = file(var.ssh-public-key)
   file_permission = "400"
 }
 
